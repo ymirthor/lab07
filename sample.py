536fa99142b99dd98973936d2f6309f7b771ffe5
ymir <ymirthorleifsson@gmail.com>
2020-03-10 18:51:30 +0000

    pytest fixed and program now works for Fizz

--- sample.py
+++ sample.py
@@ -3,27 +3,27 @@ def fizz_buzz(integer):
     #     return "FizzBuzz"
     # elif integer % 5:
     #     return "Buzz"
-    # elif integer % 5:
-    #     return "Fizz"
+    if integer % 3:
+        return "Fizz"
     return integer
     
 def test_fizz_buzz():
     assert fizz_buzz(0) == 0
-    assert fizz_buzz(3) == "Buzz"
-    assert fizz_buzz(5) == "Fizz"
+    assert fizz_buzz(3) == "Fizz"
+    assert fizz_buzz(5) == "Buzz"
     assert fizz_buzz(15) == "FizzBuzz"
     
     assert fizz_buzz("lol") == 0
     assert fizz_buzz(2) == 2
     assert fizz_buzz(7) == 7
     
-    assert fizz_buzz(6) == "Buzz"
-    assert fizz_buzz(9) == "Buzz"
-    assert fizz_buzz(12) == "Buzz"
+    assert fizz_buzz(6) == "Fizz"
+    assert fizz_buzz(9) == "Fizz"
+    assert fizz_buzz(12) == "Fizz"
 
-    assert fizz_buzz(10) == "Fizz"
-    assert fizz_buzz(20) == "Fizz"
-    assert fizz_buzz(40) == "Fizz"
+    assert fizz_buzz(10) == "Buzz"
+    assert fizz_buzz(20) == "Buzz"
+    assert fizz_buzz(40) == "Buzz"
 
     assert fizz_buzz(30) == "FizzBuzz"
     assert fizz_buzz(45) == "FizzBuzz"
