b95be636de7f8a85c3e1a9b90a69b058f3e123cc
ymir <ymirthorleifsson@gmail.com>
2020-03-10 18:43:18 +0000

    first test created

--- sample.py
+++ sample.py
@@ -1,8 +1,11 @@
-def FizzBuzz(integer):
-    if integer % 3 == 0 and integer % 5 == 0:
-        return "FizzBuzz"
-    elif integer % 5:
-        return "Buzz"
-    elif integer % 5:
-        return "Fizz"
+def fizz_buzz(integer):
+    # if integer % 3 == 0 and integer % 5 == 0:
+    #     return "FizzBuzz"
+    # elif integer % 5:
+    #     return "Buzz"
+    # elif integer % 5:
+    #     return "Fizz"
     return integer
+    
+def test_fizz_buzz():
+    assert fizz_buzz(0) == 0
