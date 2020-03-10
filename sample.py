6a312a1a31887cb32feb507a952d4e0737c73d98
ymir <ymirthorleifsson@gmail.com>
2020-03-10 18:48:01 +0000

    more detailed test added

--- sample.py
+++ sample.py
@@ -9,3 +9,22 @@ def fizz_buzz(integer):
     
 def test_fizz_buzz():
     assert fizz_buzz(0) == 0
+    assert fizz_buzz(3) == "Buzz"
+    assert fizz_buzz(5) == "Fizz"
+    assert fizz_buzz(15) == "FizzBuzz"
+    
+    assert fizz_buzz("lol") == 0
+    assert fizz_buzz(2) == 2
+    assert fizz_buzz(7) == 7
+    
+    assert fizz_buzz(6) == "Buzz"
+    assert fizz_buzz(9) == "Buzz"
+    assert fizz_buzz(12) == "Buzz"
+
+    assert fizz_buzz(10) == "Fizz"
+    assert fizz_buzz(20) == "Fizz"
+    assert fizz_buzz(40) == "Fizz"
+
+    assert fizz_buzz(30) == "FizzBuzz"
+    assert fizz_buzz(45) == "FizzBuzz"
+    assert fizz_buzz(60) == "FizzBuzz"
